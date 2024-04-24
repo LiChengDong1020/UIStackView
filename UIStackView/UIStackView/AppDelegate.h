fa4d5767cb0113efaa40362a5da6d8d0947f8067
lichengdong1 <13521828186@163.com>
2024-04-24 11:19:21 +0800

    初始化UIStackView项目

---
+++ UIStackView/UIStackView/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  UIStackView
+//
+//  Created by lichengdong on 2024/4/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ UIStackView/UIStackView/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  UIStackView
+//
+//  Created by lichengdong on 2024/4/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
